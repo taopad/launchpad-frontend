b55fbe94129aeb5dc4cc1d93fa190c8fd11a0b66
niera26 <niera26@outlook.com>
2024-02-23 14:38:50 +0100

    use spaces for indentation

--- app/page.tsx
+++ app/page.tsx
@@ -9,76 +9,76 @@ import { ProjectTokenAddress } from "@/components/ProjectTokenAddress";
 import { ProjectCoverImage } from "@/components/ProjectCoverImage";
 
 export default function Home() {
-	return (
-		<WalletProvider chainId={1}>
-			<div className="flex flex-col gap-8">
-				<Navbar />
-				<div className="flex flex-col gap-8 px-8 max-w-[1024px] w-full mx-auto">
-					<ProjectProvider
-						chainId={1}
-						address="0x481dacbf63363d142d761C6DE51da05F10A2b70D"
-					>
-						<ProjectCoverImage />
-					</ProjectProvider>
+    return (
+        <WalletProvider chainId={1}>
+            <div className="flex flex-col gap-8">
+                <Navbar />
+                <div className="flex flex-col gap-8 px-8 max-w-[1024px] w-full mx-auto">
+                    <ProjectProvider
+                        chainId={1}
+                        address="0x481dacbf63363d142d761C6DE51da05F10A2b70D"
+                    >
+                        <ProjectCoverImage />
+                    </ProjectProvider>
 
-					<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-						<ProjectProvider
-							chainId={1}
-							address="0x481dacbf63363d142d761C6DE51da05F10A2b70D"
-						>
-							<Card className="border p-7 bg-black shadow-lg rounded-lg">
-								<h2 className="text-3xl text-white font-bold mb-3">
-									<ProjectName />
-								</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <ProjectProvider
+                            chainId={1}
+                            address="0x481dacbf63363d142d761C6DE51da05F10A2b70D"
+                        >
+                            <Card className="border p-7 bg-black shadow-lg rounded-lg">
+                                <h2 className="text-3xl text-white font-bold mb-3">
+                                    <ProjectName />
+                                </h2>
 
-								<p className="text-base mb-4 border-b pb-4 ">
-									Address:
-									<ProjectTokenAddress />
-								</p>
-								<Button
-									variant="secondary"
-									className="w-full no-underline"
-									asChild
-								>
-									<Link
-										href="/projects/1/0x116Bb71512f8E9f76Df2840C7CED2B5Ee06f9C85"
-										target="_blank"
-									>
-										TBANK Seed round (1000+ TPAD)
-									</Link>
-								</Button>
-							</Card>
-						</ProjectProvider>
-						<ProjectProvider
-							chainId={1}
-							address="0x116Bb71512f8E9f76Df2840C7CED2B5Ee06f9C85"
-						>
-							<Card className="border p-7 bg-black shadow-lg rounded-lg">
-								<h2 className="text-3xl text-white font-bold mb-3">
-									<ProjectName />
-								</h2>
+                                <p className="text-base mb-4 border-b pb-4 ">
+                                    Address:
+                                    <ProjectTokenAddress />
+                                </p>
+                                <Button
+                                    variant="secondary"
+                                    className="w-full no-underline"
+                                    asChild
+                                >
+                                    <Link
+                                        href="/projects/1/0x116Bb71512f8E9f76Df2840C7CED2B5Ee06f9C85"
+                                        target="_blank"
+                                    >
+                                        TBANK Seed round (1000+ TPAD)
+                                    </Link>
+                                </Button>
+                            </Card>
+                        </ProjectProvider>
+                        <ProjectProvider
+                            chainId={1}
+                            address="0x116Bb71512f8E9f76Df2840C7CED2B5Ee06f9C85"
+                        >
+                            <Card className="border p-7 bg-black shadow-lg rounded-lg">
+                                <h2 className="text-3xl text-white font-bold mb-3">
+                                    <ProjectName />
+                                </h2>
 
-								<p className="text-base mb-4 border-b pb-4">
-									Address:
-									<ProjectTokenAddress />
-								</p>
-								<Button
-									variant="secondary"
-									className="w-full no-underline"
-									asChild
-								>
-									<Link
-										href="/projects/1/0x481dacbf63363d142d761C6DE51da05F10A2b70D"
-										target="_blank"
-									>
-										TBANK Tpad round (50+ TPAD)
-									</Link>
-								</Button>
-							</Card>
-						</ProjectProvider>
-					</div>
-				</div>
-			</div>
-		</WalletProvider>
-	);
+                                <p className="text-base mb-4 border-b pb-4">
+                                    Address:
+                                    <ProjectTokenAddress />
+                                </p>
+                                <Button
+                                    variant="secondary"
+                                    className="w-full no-underline"
+                                    asChild
+                                >
+                                    <Link
+                                        href="/projects/1/0x481dacbf63363d142d761C6DE51da05F10A2b70D"
+                                        target="_blank"
+                                    >
+                                        TBANK Tpad round (50+ TPAD)
+                                    </Link>
+                                </Button>
+                            </Card>
+                        </ProjectProvider>
+                    </div>
+                </div>
+            </div>
+        </WalletProvider>
+    );
 }
